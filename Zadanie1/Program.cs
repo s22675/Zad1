c1038e8ce8386f9d0925fe47c8aa6bda5de1411d
s22675 <s22675@pjwstk.edu.pl>
2024-03-23 22:11:56 +0100

    Create method CalculateAverage

--- Zadanie1/Program.cs
+++ Zadanie1/Program.cs
@@ -1,3 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Modifikacja 3");
+static double CalculateAverage(int[] array)
+{
+    if (array == null)
+    {
+        throw new ArgumentException("Tablica nie moze byc pusta");
+    }
+    
+    int sum = 0;
+    
+    foreach (int num in array)
+        sum += num;
+
+    return (double) sum / array.Length;
+}
+
+int[] numbers = {};
+double average = CalculateAverage(numbers);
+Console.WriteLine("Średnia: " + average);
